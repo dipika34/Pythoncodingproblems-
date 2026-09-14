c4048eeb5ff910e5eea28c8a3a830120853180c4
dipika34 <dipikapaik.10.u.4.2020@gmail.com>
2026-09-14 23:56:02 +0530

    Create userList.py

---
+++ userList.py
@@ -0,0 +1,14 @@
+nums=int(input("How many elements?"))
+list1 = []
+print("Enter the numbers:")
+for i in range(nums):
+    el = int(input())
+
+    list1.append(el)
+
+print("There are n items in the list\n",list1)
+
+
+
+
+
